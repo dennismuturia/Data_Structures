60388daeb134fc20f3e126fa951373b1d511d010
dennismuturia <dmuturia2922@gmail.com>
2018-07-05 21:08:03 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+//This is for a list
+ using namespace std;
+
+struct  Node{
+    int data;
+    Node* link;
+};
+
+int main() {
+    /*
+     * Beginning of linked lists
+    Node* A;//Emptylist
+    A = nullptr;//Start of the linked list
+
+    //Create a node
+    //we need to return the location of memory because malloc returns void
+    Node* temp =  (Node*)malloc(sizeof(Node));//Memory reference
+
+    temp->data = 2;
+    //Creating a list dynamically
+    Node* temp1 = A;
+
+    //TRaversal of the list until the end of list
+    while(temp1->link != nullptr){
+        temp1 = temp1->link;
+    }
+    temp1->link = temp;
+    temp->link = temp;
+    A =  temp;
+
+    temp = new Node();
+    temp->data = 4;
+    temp->link = nullptr;
+
+
+    return 0;
+     */
+    /*
+     * LInked list at the beginning of a linked list
+     */
+}
